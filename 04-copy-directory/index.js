e519c3c215cc76d983bf9ecf31915f4c625f921f
SeriakovaOksana <hikki0019@gmail.com>
2023-05-05 13:12:53 +0300

    feat: add solution of 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDirectory(dirFrom, dirTo) {
+    await fs.promises.mkdir(dirTo, {recursive: true});
+
+    const files = await fs.promises.readdir(dirFrom);
+
+    for (let file of files) {
+        const dirFromPath = path.join(dirFrom, file);
+        const dirToPath = path.join(dirTo, file);
+
+        const stat = await fs.promises.stat(dirFromPath);
+
+        if (stat.isFile()) {
+            const readStream = fs.createReadStream(dirFromPath);
+            const writeStream = fs.createWriteStream(dirToPath);
+
+            readStream.pipe(writeStream);
+        } else if (stat.isDirectory()) {
+            await copyDirectory(dirFromPath, dirToPath)
+        }
+    }
+}
+
+copyDirectory('./04-copy-directory/files', './04-copy-directory/files-copy');
